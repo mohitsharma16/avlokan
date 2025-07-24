565250a6c3fbf957e1a08a818bdc89328cd921d8
mohits <mohit.sharma161203@gmail.com>
2025-07-24 14:42:47 +0530

    feat: add routes for the pages

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/pages/home.tsx"),
+  route("revision/:id", "routes/pages/revision.$id.tsx"),
+  route("*", "routes/pages/notfound.tsx"),
+] satisfies RouteConfig;

b343f9629be8c0a1e565083849376095ad536086
mohits <mohit.sharma161203@gmail.com>
2025-07-24 18:57:38 +0530

    fix: auth gate issue fix

--- app/routes/components/Auth/AuthGate.tsx
+++ app/routes/components/Auth/AuthGate.tsx
@@ -1,12 +1,17 @@
-import { useAuth } from "../../contexts/AuthContext";
-import AuthForm from "./AuthForm";
+import { useMatches, Navigate } from "react-router-dom";
+import { useAuth } from "~/routes/contexts/AuthContext";
 
 export default function AuthGate({ children }: { children: React.ReactNode }) {
-  const { isLoggedIn } = useAuth();
+  const { user } = useAuth();
+  const matches = useMatches();
 
-  if (!isLoggedIn) {
-    return <AuthForm />;
+  const isPublicRoute = matches.some((match) => (match.handle as any)?.public);
+
+  if (isPublicRoute) {
+    return <>{children}</>;
   }
-
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
   return <>{children}</>;
 }

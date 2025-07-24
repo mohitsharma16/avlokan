a48c6ac67a7477923bc84d382bf1b9297f980187
mohits <mohit.sharma161203@gmail.com>
2025-07-24 17:12:35 +0530

    feat: add not found page for the app

---
+++ app/routes/pages/notfound.tsx
@@ -0,0 +1,9 @@
+export const handle = { public: true };
+export default function () {
+  return (
+    <div className="w-full h-full min-h-svh flex flex-col items-center justify-center">
+      <h2 className="text-3xl font-semibold">404</h2>
+      <span>Couldn't find what you were looking for</span>
+    </div>
+  );
+}

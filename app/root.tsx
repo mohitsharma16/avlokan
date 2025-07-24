53e8e2734113a983b2ba560f6095549762c238f5
mohits <mohit.sharma161203@gmail.com>
2025-07-24 14:42:31 +0530

    refactor: update routes

--- app/root.tsx
+++ app/root.tsx
@@ -5,10 +5,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useMatches, 
 } from "react-router";
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import AuthGate from "./routes/components/Auth/AuthGate";
+import { AuthProvider } from "./routes/contexts/AuthContext";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -41,8 +44,29 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+
 export default function App() {
-  return <Outlet />;
+  const matches = useMatches();
+
+  
+  const routeHandle = matches[matches.length - 1]?.handle as
+    | { public?: boolean }
+    | undefined;
+
+  
+  const isPublicRoute = routeHandle?.public === true;
+
+  return (
+    <AuthProvider>
+      {isPublicRoute ? (
+        <Outlet /> 
+      ) : (
+        <AuthGate>
+          <Outlet /> 
+        </AuthGate>
+      )}
+    </AuthProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
